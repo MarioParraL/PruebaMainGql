5adf2ba284b11eb2553a1619178e2ae95a46de9a
MarioParraL <mariovidovick@gmail.com>
2025-01-30 13:53:08 +0100

    main

---
+++ main.ts
@@ -0,0 +1,29 @@
+import { MongoClient } from "mongodb";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const MONGO_URL = Deno.env.get("MONGO_URL");
+if (!MONGO_URL) {
+  throw new Error("Please provide a MONGO_URL");
+}
+
+const mongoClient = new MongoClient(MONGO_URL);
+await mongoClient.connect();
+console.info("Connected to MongoDB");
+
+const mongoDB = mongoClient.db("MainDB");
+const HolaCollection = mongoDB.collection("hola");
+
+/*
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  context: async () => ({ HolaCollection }),
+});
+
+console.info(`Server ready at ${url}`);
+
+*/
